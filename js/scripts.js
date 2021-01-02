43c7c24aac4ed3621b87b5ad3239f0507d733c8f
Tatum Bray <tatumrbray@gmail.com>
2021-01-02 09:36:51 -0700

    jquery modals, sass, organized code

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,15 @@ $(function(){
         }
     });
 });
+
+$(function(){
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('show');
+    })
+})
+
+$(function(){
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('show');
+    })
+})
